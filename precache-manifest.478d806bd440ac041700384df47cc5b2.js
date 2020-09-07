6899ca8a92ce6cb451e26354221c3838879dcb0c
nguyenhuutuong999 <nguyenhuutuong999@gmail.com>
2020-09-07 11:10:28 +0700

    Updates

---
+++ precache-manifest.478d806bd440ac041700384df47cc5b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2392b9eeedc45e88d7338b0a15ae5df5",
+    "url": "/iTask/index.html"
+  },
+  {
+    "revision": "dc86052ea13794de819b",
+    "url": "/iTask/static/css/main.b13e0ec3.chunk.css"
+  },
+  {
+    "revision": "8d153439d98d0c1d1106",
+    "url": "/iTask/static/js/2.bc9a0d39.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/iTask/static/js/2.bc9a0d39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc86052ea13794de819b",
+    "url": "/iTask/static/js/main.e6c12117.chunk.js"
+  },
+  {
+    "revision": "d94462706f5987c7fec7",
+    "url": "/iTask/static/js/runtime-main.75ebf910.js"
+  }
+]);
